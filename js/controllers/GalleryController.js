4f78938787d86ee21e598e89a775c77f9d6aa40e
Donny <1021001390@qq.com>
2017-04-14 17:11:06 +0800

    head and side menu of gallery

--- js/controllers/GalleryController.js
+++ js/controllers/GalleryController.js
@@ -5,107 +5,154 @@
     'use strict';
 
     angular.module('portals.controllers')
-        .controller('GalleryController', ['$scope', function ($scope) {
-            $scope.vm = {
-                title: '资源',
+        .controller('GalleryController', ['$scope', '$timeout', function ($scope, $timeout) {
+            var vm = $scope.vm = {
+                title: 'Living Atlas 中的专题地图',
                 sections: [
                     {
                         id: 0,
-                        name: '分类1'
+                        name: '影像'
                     }, {
                         id: 1,
-                        name: '分类2'
+                        name: '底图'
                     }, {
                         id: 2,
-                        name: '分类3'
+                        name: '历史地图'
                     }, {
                         id: 3,
-                        name: '分类4'
+                        name: '人口统计与生活方式'
                     }, {
                         id: 4,
-                        name: '分类5'
-                    }],
-                selected: 0,
+                        name: '景观'
+                    }, {
+                        id: 5,
+                        name: '海洋'
+                    }, {
+                        id: 6,
+                        name: '地球观测'
+                    }, {
+                        id: 7,
+                        name: '城市系统'
+                    }, {
+                        id: 8,
+                        name: '运输'
+                    }, {
+                        id: 9,
+                        name: '边界和地点'
+                    }, {
+                        id: 10,
+                        name: '地图故事'
+                    }
+                ],
+                selected: {
+                    id: 0,
+                    value: '最新地图'
+                },
                 options: [{
                     id: 0,
-                    value: ''
+                    value: '最新地图'
+                }, {
+                    id: 1,
+                    value: 'Title'
+                }, {
+                    id: 2,
+                    value: 'Visited'
                 }],
                 display: 0,
                 gallery: [
                     {
                         id: 0,
-                        title: 'Mid-Century Map',
+                        title: '四维导航图',
                         author: 'Web Map',
                         img: '../images/map1.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 1,
-                        title: 'Mid-Century Map',
+                        title: '四维导航图',
                         author: 'Web Map',
                         img: '../images/map2.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 2,
-                        title: 'Mid-Century Map',
+                        title: '四维导航图',
                         author: 'Web Map',
                         img: '../images/map3.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 3,
-                        title: 'Mid-Century Map',
+                        title: '四维导航图',
                         author: 'Web Map',
                         img: '../images/map4.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 4,
-                        title: 'Mid-Century Map',
+                        title: '北京市迁徙图',
                         author: 'Web Map',
                         img: '../images/map5.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 5,
-                        title: 'Mid-Century Map',
+                        title: '北京市迁徙图',
                         author: 'Web Map',
                         img: '../images/map6.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 6,
-                        title: 'Mid-Century Map',
+                        title: '北京市迁徙图',
                         author: 'Web Map',
                         img: '../images/map7.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 7,
-                        title: 'Mid-Century Map',
+                        title: '北京市迁徙图',
                         author: 'Web Map',
                         img: '../images/map8.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 8,
-                        title: 'Mid-Century Map',
+                        title: '公交线路图',
                         author: 'Web Map',
                         img: '../images/map9.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 9,
-                        title: 'Mid-Century Map',
+                        title: '公交线路图',
                         author: 'Web Map',
                         img: '../images/map10.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 10,
-                        title: 'Mid-Century Map',
+                        title: '公交线路图',
                         author: 'Web Map',
                         img: '../images/map11.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }, {
                         id: 11,
-                        title: 'Mid-Century Map',
+                        title: '公交线路图',
                         author: 'Web Map',
                         img: '../images/map12.jpg',
                         description: "this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing.this is this is this some thing."
                     }],
+                droplist: false,
                 expand: false
+            };
+
+            $scope.change = function (option) {
+                console.log(option);
+            };
+
+            $scope.focus = function () {
+                $scope.$evalAsync(function () {
+                    console.log("focus");
+                });
+            };
+
+            $scope.blur = function () {
+                $scope.$evalAsync(function ($scope) {
+                    $scope.vm.droplist = false;
+                    console.log($scope.vm.droplist);
+                });
+                console.log("blur");
             };
         }]);
 })(angular);
